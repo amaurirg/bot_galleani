cea56227079d0df0745c522e08ae642609cfe4f3
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2017-12-06 20:18:53 -0200

    Alterando a view para testar

--- bot_webhook/core/views.py
+++ bot_webhook/core/views.py
@@ -13,9 +13,11 @@ def home(request):
 @csrf_exempt
 def event(requests):
     json_list = json.loads(requests.body)
+    # print(json_list)
     first_name = json_list['message']['chat']['first_name']
     chat_id = json_list['message']['chat']['id']
     text_message = json_list['message']['text']
     print(chat_id)
     send_message('{}, sua mensagem ao contrário é\n{}'.format(first_name, text_message[::-1]), chat_id)
+    # return JsonResponse({'status': 'true', 'message': 'worked'})
     return HttpResponse()
